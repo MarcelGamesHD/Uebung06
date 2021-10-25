bc50d520236e017851cba2071087637f862fe724
Janetzky.Marcel <Janetzky.Marcel@HERTZ.local>
2021-10-25 14:34:02 +0200

    Projektdateien hinzufügen.

---
+++ 2021_10_25_verzweigung/Aufgabe1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2021_10_25_verzweigung
+{
+    class DatentypenFormateConvert
+    {
+        static void Main(string[] args)
+        {
+            int zahl;
+            Console.WriteLine("Bitte geben Sie eine Ganzzahl ein: ");
+            zahl = Convert.ToInt32(Console.ReadLine());
+            if (zahl == 0)
+            {
+                Console.Write("Sie haben eine 0 eingegeben!");
+            }
+            else
+                if (zahl % 2 == 0 && zahl != 0)
+            {
+                Console.Write("Sie haben eine gerade Zahl eingegeben!");
+            }
+                else
+            {
+                Console.Write("Sie haben eine ungerade Zahl eingegeben!");
+            }
+        }
+    }
+}
